1f55224d8d199f60f6cb426bd82b97b6a8c1fbbc
Rebecca <smartycope@gmail.com>
2021-09-11 10:48:48 -0600

    cleaned the repo, so the gitignore SHOULD work now

--- Qml/build/CMakeFiles/sap_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sap_autogen"
-  "sap_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sap_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
